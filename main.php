4baa60d218ae8f71f3fb0ffc0f985f274c315f4e
commoodor <160464710+commoodor@users.noreply.github.com>
2024-03-01 22:46:35 +0700

    dev

--- main.php
+++ main.php
@@ -1,20 +1,67 @@
 <?php
 // Fetch data from the URL
-$url = "https://luckpool.net/verus/miner/RVxwfn5TggLnYPgEAGQf8W7kes28QNQGJg";
+$url = "https://luckpool.net/verus/miner/RM4X75bwroSHCLG7W7zXZ8a2m19xgkrChk";
 $response = file_get_contents($url);
 $data = json_decode($response, true);
 
-// Process and format the data
+// Extract and sort worker data
+$workers = $data['workers'];
+sort($workers);
+
+// Display worker
 $formatted_data = "";
-foreach ($data['workers'] as $idx => $worker) {
-    $formatted_data .= ($idx + 1) . " - " . $worker['name'] . " - " . $worker['hashrate'] . " - " . $worker['status'] . "\n";
+foreach ($workers as $index => $worker) {
+    $workerData = explode(':', $worker);
+    
+    // Fetch additional data for each worker
+    $workerUrl = 'https://luckpool.net/verus/worker/' . $data['address'] . '.' . $workerData[0];
+    $workerJson = file_get_contents($workerUrl);
+    $workerInfo = json_decode($workerJson, true);
+    
+    // Display worker data
+    echo "<tr>";
+    echo "<td>" . ($index + 1) . "</td>";
+    echo "<td>" . $workerData[3] . "</td>"; // Status
+    echo "<td>" . $workerData[0] . "</td>"; // ID
+    echo "<td>" . $workerData[1] . "</td>"; // Hashrate
+    echo "<td>" . $workerInfo['stratumServer'] . "</td>"; // Stratum Server
+   
+    // Display Software
+    echo "<td>" . $workerInfo['software'] . "</td>";
+    echo "</tr>";
+
+    // Process and format the data
+$formatted_data .=
+($index + 1) . " | " .
+$workerData[3] . " - " .
+$workerData[0] . " - " .
+$workerInfo['hashrateString'] . " - " .
+$workerInfo['stratumServer'] . " - " .
+$workerInfo['software'] . "\n";
+
+
 }
+//echo "</table>";
+
 
 // Send data to Telegram
 $telegram_bot_token = getenv('TELEGRAM_BOT_TOKEN');
-$telegram_chat_id = getenv('TELEGRAM_CHAT_ID');
+$telegram_chat_id = getenv('TELEGRAM_CHAT_ID1');
 $telegram_api_url = "https://api.telegram.org/bot$telegram_bot_token/sendMessage";
-$message = "Address : " . $data['address'] . "\nHashrate : " . $data['total_hashrate'] . "\n\nNo | Worker | Hashrate | Status\n" . $formatted_data;
+$message = "
+
+Report Date : " . date('d-m-Y H:i:s', $data['timestamp'] + 25200) . "\n\n
+Address : " . $data['address'] . "\n
+Hashrate : " . $data['hashrateString'] . "\n\n
+Estimated Luck : " . $data['estimatedLuck'] . "\n
+Efficiency : " . $data['efficiency'] . "%\n\n
+Immature : " . $data['immature'] . "\n
+Balance : " . $data['balance'] . "\n\n
+
+# | Status | ID | Hashrate | Stratum | Miner \n" . 
+$formatted_data;
+
+
 $payload = [
     'chat_id' => $telegram_chat_id,
     'text' => $message
