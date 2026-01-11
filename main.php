b7f0c5d1d2ee4e379424a32d03eb9b451c598e72
commoodor <160464710+commoodor@users.noreply.github.com>
2026-01-11 21:09:59 +0700

    Update User-Agent header in main.php

--- main.php
+++ main.php
@@ -53,7 +53,7 @@ function estimatedpaid($amount) {
     }
 
     $headers = [
-        "User-Agent: VerusPoolBot/1.0 (+https://github.com/commoodor/lvckpool)",
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
         "Accept: application/json"
     ];
 
